9efbf5c28d1ea956df79f724cee89c7cd95a45c1
Wiktoria Kostyla <wkostyla@c3r5s1.42warsaw.pl>
2025-02-07 17:50:58 +0100

    memory allocation

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,37 @@
 /*   By: wkostyla <wkostyla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 16:36:59 by wkostyla          #+#    #+#             */
-/*   Updated: 2025/02/07 16:38:36 by wkostyla         ###   ########.fr       */
+/*   Updated: 2025/02/07 17:48:35 by wkostyla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *read_fd(int fd, char* result)
+char *read_fd(int fd, char* temp)
 {
     char *buffer;
     int bytes;
 
-    if (!result)
-        result = ft_calloc(1, 1);
+    if (!temp)
+        temp = ft_calloc(1, 1);
+    buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+    bytes = 1;
+    while (bytes > 1)
+    {
+        bytes = read(fd, buffer, BUFFER_SIZE);
+        if (bytes == -1)
+        {
+            free(buffer);
+            return (NULL);
+        }
+        buffer[bytes] = 0;
+        //join and free ???????????????/
+        if (ft_strchr(buffer, '\n'))
+            break;
+    }
+    
+    free(buffer);
+    return(temp);
 }
 
 char* get_next_line(int fd)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: wkostyla <wkostyla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 15:42:01 by wkostyla          #+#    #+#             */
-/*   Updated: 2025/02/07 16:37:39 by wkostyla         ###   ########.fr       */
+/*   Updated: 2025/02/07 17:13:44 by wkostyla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,22 @@ void	*ft_calloc(size_t nmemb, size_t size)
 		return (NULL);
 	ft_bzero(ptr, (nmemb * size));
 	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+		{
+			return ((char *)(s + i));
+		}
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)(s + i));
+	return (NULL);
 }
